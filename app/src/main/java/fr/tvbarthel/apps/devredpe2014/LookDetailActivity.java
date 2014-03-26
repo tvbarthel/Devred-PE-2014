f62f49f5a749297d02d749a115d714307d29ce3c
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-03-26 23:06:22 +0100

    Factorize a bit

--- app/src/main/java/fr/tvbarthel/apps/devredpe2014/LookDetailActivity.java
+++ app/src/main/java/fr/tvbarthel/apps/devredpe2014/LookDetailActivity.java
@@ -1,13 +1,11 @@
 package fr.tvbarthel.apps.devredpe2014;
 
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewTreeObserver;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -15,7 +13,6 @@ import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import fr.tvbarthel.apps.devredpe2014.model.Look;
-import fr.tvbarthel.apps.devredpe2014.model.LookItem;
 import fr.tvbarthel.apps.devredpe2014.ui.ActionBarHelper;
 import fr.tvbarthel.apps.devredpe2014.ui.LookItemAdapter;
 import uk.co.senab.photoview.PhotoViewAttacher;
@@ -27,6 +24,7 @@ public class LookDetailActivity extends Activity {
     private Look mLook;
     private ImageView mImageView;
     private ListView mListView;
+    private LookItemAdapter mAdapter;
     private PhotoViewAttacher mAttacher;
 
     @Override
@@ -34,50 +32,7 @@ public class LookDetailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_look_detail);
         ActionBarHelper.setDevredTitle(this, getActionBar());
-
-        mLook = getIntent().getParcelableExtra(EXTRA_LOOK);
-        if (mLook == null) {
-            finish();
-        } else {
-            init();
-        }
-    }
-
-    private void init() {
-        mListView = (ListView) findViewById(R.id.test);
-        mImageView = (ImageView) findViewById(R.id.look_detail_activity_look_image);
-        mAttacher = new PhotoViewAttacher(mImageView);
-
-        Picasso.with(this).load(mLook.getLookResourceId()).skipMemoryCache().into(mImageView, new Callback() {
-            @Override
-            public void onSuccess() {
-                mAttacher.update();
-                mAttacher.zoomTo(1.2f, 0, 0);
-            }
-
-            @Override
-            public void onError() {
-
-            }
-        });
-
-        final View headerView = getLayoutInflater().inflate(R.layout.header_look_detail, mListView, false);
-        final ArrayAdapter<LookItem> adapter = new LookItemAdapter(LookDetailActivity.this, mLook.getLookItems());
-        mListView.addHeaderView(headerView);
-        final View contentView = findViewById(android.R.id.content);
-        final int headerListHeight = getResources().getDimensionPixelSize(R.dimen.header_look_detail_height);
-        contentView.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-            @Override
-            public boolean onPreDraw() {
-                contentView.getViewTreeObserver().removeOnPreDrawListener(this);
-                int height = contentView.getMeasuredHeight();
-                int paddingTop = height - headerListHeight;
-                mListView.setPadding(mListView.getPaddingLeft(), paddingTop, mListView.getPaddingRight(),
-                        mListView.getPaddingBottom());
-                mListView.setAdapter(adapter);
-                return true;
-            }
-        });
+        loadLookOrFinish();
     }
 
     @Override
@@ -97,6 +52,58 @@ public class LookDetailActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void loadLookOrFinish() {
+        mLook = getIntent().getParcelableExtra(EXTRA_LOOK);
+        if (mLook == null) {
+            finish();
+        } else {
+            init();
+        }
+    }
+
+    private void init() {
+        mListView = (ListView) findViewById(R.id.test);
+        mImageView = (ImageView) findViewById(R.id.look_detail_activity_look_image);
+        mAttacher = new PhotoViewAttacher(mImageView);
+        mAdapter = new LookItemAdapter(LookDetailActivity.this, mLook.getLookItems());
+        final View headerView = getLayoutInflater().inflate(R.layout.header_look_detail, mListView, false);
+        mListView.addHeaderView(headerView);
+
+        loadLookImage();
+        initListViewPaddingTop();
+    }
+
+    private void loadLookImage() {
+        Picasso.with(this).load(mLook.getLookResourceId()).skipMemoryCache().into(mImageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                mAttacher.update();
+                mAttacher.zoomTo(1.2f, 0, 0);
+            }
+
+            @Override
+            public void onError() {
+            }
+        });
+    }
+
+    private void initListViewPaddingTop() {
+        final int headerListHeight = getResources().getDimensionPixelSize(R.dimen.header_look_detail_height);
+        final View contentView = findViewById(android.R.id.content);
+        contentView.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                contentView.getViewTreeObserver().removeOnPreDrawListener(this);
+                int height = contentView.getMeasuredHeight();
+                int paddingTop = height - headerListHeight;
+                mListView.setPadding(mListView.getPaddingLeft(), paddingTop, mListView.getPaddingRight(),
+                        mListView.getPaddingBottom());
+                mListView.setAdapter(mAdapter);
+                return true;
+            }
+        });
     }
 
 }
