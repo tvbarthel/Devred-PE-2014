15f3a6999eaeaa182103061ae6f72eb98c7935c6
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-03-28 22:27:30 +0100

    Remove menu from the look detail activity

--- app/src/main/java/fr/tvbarthel/apps/devredpe2014/LookDetailActivity.java
+++ app/src/main/java/fr/tvbarthel/apps/devredpe2014/LookDetailActivity.java
@@ -2,8 +2,6 @@ package fr.tvbarthel.apps.devredpe2014;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
@@ -33,25 +31,6 @@ public class LookDetailActivity extends Activity {
         setContentView(R.layout.activity_look_detail);
         ActionBarHelper.setDevredTitle(this, getActionBar());
         loadLookOrFinish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.look_detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private void loadLookOrFinish() {
