6ea23eac01ec4f104f9fc3c46148da36044eabb5
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-03-29 11:29:20 +0100

    Remove settings menu item

--- app/src/main/java/fr/tvbarthel/apps/devredpe2014/MainActivity.java
+++ app/src/main/java/fr/tvbarthel/apps/devredpe2014/MainActivity.java
@@ -67,9 +67,6 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         boolean isSelectionConsume;
         switch (id) {
